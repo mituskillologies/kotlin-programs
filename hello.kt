f13c4bf10b54a98f028081df2a3abb8e2d55dc4a
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-24 07:54:39 +0530

    Add files via upload

---
+++ hello.kt
@@ -0,0 +1,9 @@
+/* My first program
+   Date: 10/10/2025
+   Problem: Print something on the screen
+*/
+fun main() {
+	println("Welcome to Kotlin.")    // This is my first statement!
+	//println("Hello World!")
+	println("Good Bye!")
+}
