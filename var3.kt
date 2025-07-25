b1cb3193c209806ee7b4d49f88ed0a0ba132307e
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-25 07:54:21 +0530

    Add files via upload

---
+++ var3.kt
@@ -0,0 +1,10 @@
+// Variable declaration with data type
+fun main() {
+	var name = "Maharashtra"
+	var num = 1234
+	var Name = "Tushar"
+	
+	println("Name:" + name)
+	println("Number:" + num)
+	println("Name:" + Name)
+}
