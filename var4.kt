bbefe80197f2520e85bd0c1a27948352068fb788
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-25 07:54:30 +0530

    Add files via upload

---
+++ var4.kt
@@ -0,0 +1,15 @@
+fun main() {
+	var b: Byte = 100
+	var s: Short = 100
+	var i: Int = 100
+	var l: Long = 100
+	var f: Float = 100.12f
+	var d: Double = 100.12
+	
+	println("Byte is: "+ b)
+	println("Short is: "+ s)
+	println("Int is: "+ i)
+	println("Long is: "+ l)
+	println("Float is: "+ f)
+	println("Double is: "+ d)
+}
