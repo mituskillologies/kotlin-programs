bd31e7a8a6b8469ed22701293ee4f6cb2a09d020
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-26 18:10:28 +0530

    Add files via upload

---
+++ mutable.kt
@@ -0,0 +1,16 @@
+// Mutable variables
+fun main()  {
+	var name = "Tushar"
+	var marks = 68.45
+	var total = 78
+	
+	println("My name:" + name)
+	println("Marks:" + marks)
+	println("Total:" + total)
+	
+	name = "Pune"
+	total = 100
+	
+	println("My name:" + name)
+	println("Total:" + total)
+}
