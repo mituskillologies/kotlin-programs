dd98b6a467fa81f0dc3d61776c8116ff87f4233f
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-26 18:10:36 +0530

    Add files via upload

---
+++ immut.kt
@@ -0,0 +1,14 @@
+// Immutable / constant variables
+fun main() {
+	val name = "Maharashtra"
+	val num = 1234
+	
+	println("Name:" + name)
+	println("Number:" + num)
+	
+	name = "Pune"
+	num = 4567
+	
+	println("Name:" + name)
+	println("Number:" + num)
+}
