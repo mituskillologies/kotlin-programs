9bc5de0759058958c33747ce64dfdb32c71b71fa
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-27 07:41:43 +0530

    Add files via upload

---
+++ str.kt
@@ -0,0 +1,10 @@
+// String operations
+fun main() {
+	var name = "Hello, how are you?"   // single line string
+	var text = """Hello friends
+	Welcome to thw world of Kotlin Programming
+	Good Bye!"""                      // Multi line strings 
+	
+	println("Single Line: " + name)
+	println("Multi Line: " + text)
+}
