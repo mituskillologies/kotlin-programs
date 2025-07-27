8684be20f5743a475618dd0b23a3e5d3ace611f9
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-27 07:42:09 +0530

    Add files via upload

---
+++ arithmetic.kt
@@ -0,0 +1,11 @@
+fun main() {
+	var num1 = 47.9
+	var num2 = 16
+	
+	println("Addition:" + (num1 + num2))   // Addition
+	println("Addition:" + (num1 + 15))
+	println("Subtraction:" + (num1 - num2))   // Subtraction
+	println("Multiplication:" + (num1 * num2))   // Multiplication
+	println("Division:" + (num1 / num2))   // Division
+	println("Modulo Division:" + (num1 % num2))   // Modulo Division
+}
