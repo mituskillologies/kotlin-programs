da3aa686ff8fa8500eb05c5a0e370a009ff3fa51
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-28 07:48:00 +0530

    Add files via upload

---
+++ relational.kt
@@ -0,0 +1,12 @@
+fun main() {
+	var num1 = 47
+	var num2 = 16
+	
+	println("Values are:"+num1+", "+num2)
+	println("Less Than:" + (num1 < num2))
+	println("Less Than/Equal:" + (num1 <= num2))
+	println("Greater Than:" + (num1 > num2))
+	println("Greater Than/Equal:" + (num1 >= num2))
+	println("Equal:" + (num1 == num2))
+	println("Not Equal:" + (num1 != num2))
+}
