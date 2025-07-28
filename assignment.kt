2387421c3521de235b574a69546adb1815fc3254
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-28 07:48:08 +0530

    Add files via upload

---
+++ assignment.kt
@@ -0,0 +1,16 @@
+fun main() {
+	var num1 = 47
+	var num2 = 47
+	
+	num1 += 3
+	println("Value of num1: " + num1)
+	
+	num1 -= 3
+	println("Value of num1: " + num1)
+	
+	num1 *= 3
+	println("Value of num1: " + num1)
+
+	num1 /= 3
+	println("Value of num1: " + num1)
+}
