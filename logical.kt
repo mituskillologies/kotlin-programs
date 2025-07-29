d74036c2725f9befabd477c5393fb8af8fe69716
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-29 08:07:35 +0530

    Add files via upload

---
+++ logical.kt
@@ -0,0 +1,8 @@
+// Logical Operators
+fun main() {
+	var num1 = 47    
+	var num2 = 19 
+
+	println("Within 0 -> 100: " + (num1 >= 0 && num1 <= 100))	// both conditions -> true
+	println("Check for num2: " + (num2 < 10 || num2 > 50)) // anyone of conditions -> true
+}   
