ce26b021908f0eb0b4b265f3d31ab41c85987456
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-07-31 08:39:01 +0530

    Add files via upload

---
+++ if3.kt
@@ -0,0 +1,14 @@
+fun main() {
+	var num: Int = 22
+    var x = num % 2
+	
+    println("Value:" + x)
+	if (x == 0) {
+		println("Even number!")
+        println("Done!")
+	}
+	else {
+		println("Odd number!")
+        println("Complete!")
+	}
+}
