5428244dd11c8214dbced822ac791703d7857bcf
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-08-01 09:09:03 +0530

    Add files via upload

---
+++ nested-if.kt
@@ -0,0 +1,15 @@
+// Check the age is in the range of 18 to 60
+fun main() {
+	var age = 14
+	if (age >= 18) {
+        if (age <= 60) {
+            println("Yes")
+        }
+        else {
+            println("No")
+        }
+    }
+    else {
+        println("NO")
+    }
+}
