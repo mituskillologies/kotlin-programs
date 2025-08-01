b7e5863561aeb92ad911494b2a1ff3ddf097ee30
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-08-01 09:08:45 +0530

    Add files via upload

---
+++ if-ladder.kt
@@ -0,0 +1,15 @@
+fun main() {
+	var marks = 44
+	
+	if (marks >= 75) {
+		println("Distinction!")
+	}	else if (marks >= 60){
+			println("First Class")
+    }	else if (marks >= 50) {
+        	println("Second Class")
+    }	else if (marks >= 40) {
+        	println("Pass Class")
+    }	else {
+        	println("Failed!")
+    }
+}
