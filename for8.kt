f2fb27ece55cc5ab8f80975b2e1cf3a9fb02db90
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-08-05 09:11:44 +0530

    Add files via upload

---
+++ for8.kt
@@ -0,0 +1,9 @@
+// Addition of first 10 odd numbers
+fun main() {
+    var nations = arrayOf("India","China","USA","Japan","Korea")
+    println("Indices: " + nations.indices)
+    
+    for (i in nations.indices) {
+        println(nations[i])
+    }
+}
