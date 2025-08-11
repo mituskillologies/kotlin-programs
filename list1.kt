a46368a46d1e9bb821c00568ee04f905d595a73c
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-08-11 08:04:34 +0530

    Add files via upload

---
+++ list1.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+   var lst1 = listOf(56,23,17,65,78,89,"Pune")
+   var lst2 = mutableListOf(56,23,17,65,78,89)
+   println("List-1: " + lst1)
+   println("List-2: " + lst2)
+   
+   println("Element-1: " + lst1[0])
+}
