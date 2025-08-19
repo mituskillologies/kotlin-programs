2935997ac804b10aad326ba7521224828375a1fe
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-08-19 08:35:00 +0530

    Add files via upload

---
+++ map1.kt
@@ -0,0 +1,8 @@
+fun main() {
+   var map1 = mapOf("a" to "apple", "b" to "ball", "c" to "cat", "d" to "dog")
+   println(map1)
+   println("All values: " + map1.entries)
+   println("Key values: " + map1.keys)
+   println("Values: " + map1.values)
+   println("String: " + map1.toString())
+}
